bed736997c455636127e2933fcdcc19b108cae82
Abinash Kushwaha <43268588+abinash3027@users.noreply.github.com>
2024-09-18 21:45:43 +1000

    Created decryptionFunction.py

---
+++ Question 3/decryptionFunction.py
@@ -0,0 +1,64 @@
+def decrypt(text, key):
+    decrypted_text = ""
+    for char in text:
+        if char.isalpha():
+            shifted = ord(char) - key
+            if char.islower():
+                if shifted < ord('a'):
+                    shifted += 26
+            elif char.isupper():
+                if shifted < ord('A'):
+                    shifted += 26
+            decrypted_text += chr(shifted)
+        else:
+            decrypted_text += char
+    return decrypted_text
+
+# Encrypted text generated from the encryption function
+encrypted_code = """
+tybony_inevnoyr = 100
+zl_qvpg = {'xrl1': 'inyhr1', 'xrl2': 'inyhr2', 'xrl3': 'inyhr3'}
+
+qrs cebprff_ahzoref():
+    tybony tybony_inevnoyr
+    ybpny_inevnoyr = 5
+    ahzoref = [1, 2, 3, 4, 5]
+
+    juvyr ybpny_inevnoyr > 0:
+        vs ybpny_inevnoyr % 2 == 0:
+            ahzoref.erzbir(ybpny_inevnoyr)
+        ybpny_inevnoyr -= 1
+
+    erghea ahzoref
+
+zl_frg = {1, 2, 3, 4, 5, 5, 4, 3, 2, 1}
+erfhyg = cebprff_ahzoref(ahzoref=zl_frg)
+
+qrs zbqvsl_qvpg():
+    ybpny_inevnoyr = 10
+    zl_qvpg['xrl4'] = ybpny_inevnoyr
+
+zbqvsl_qvpg(5)
+
+qrs hcqngr_tybony():
+    tybony tybony_inevnoyr
+    tybony_inevnoyr += 10
+
+sbe v va enatr(5):
+    cevag(v)
+    v += 1
+
+vs zl_frg vf abg Abar naq zl_qvpg['xrl4'] == 10:
+    cevag("Pbaqvgvba zrg!")
+
+vs 5 abg va zl_qvpg:
+    cevag("5 abg sbhaq va gur qvpgvbanel!")
+
+cevag(tybony_inevnoyr)
+cevag(zl_qvpg)
+cevag(zl_frg)
+"""
+
+# Using key 13 for decryption
+decrypted_code = decrypt(encrypted_code, 13)
+print("Decrypted Code:\n", decrypted_code)
